7f46c3a7074f506013d8027601d95f3f848bcc58
FireFistofUnicorn <102551631+FireFistofUnicorn@users.noreply.github.com>
2022-04-21 20:51:44 +0300

    Add files via upload

---
+++ 3.1 Чет.Нечет.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Вы ввели чётное число");
+
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели нечётное число");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 3.3 Проверка целого числа.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Проверка_простого_числа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите целое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вы ввели число {a}");
+
+            int c = 0;
+            while (c <= a)
+            {
+                Console.WriteLine($"{a}");
+                if (a % 2 == 0 && a != 2)
+                {
+                    Console.WriteLine("К сожалению, ваше число не является простым.");
+                }
+                else if (a % Math.Sqrt (a) == 0)
+                {
+                    Console.WriteLine($"К сожалению, ваше число не является простым.");
+                }
+                else if (a % a == 0 && a % 1 == 0)
+                {
+                    Console.WriteLine($"Число {a} является простым.");
+                }
+                break;
+
+            }
+            
+        }
+    }
+}
