4907fd3d02d1dfba6956e1feea30c29ceb959598
FireFistofUnicorn <102551631+FireFistofUnicorn@users.noreply.github.com>
2022-05-08 14:40:38 +0300

    Add files via upload

---
+++ 11Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace BlackJeck2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветствую вас! Введите свое количество карт");
+            int a = int.Parse(Console.ReadLine());
+            int b = 0;
+           
+            for (int i = 1; i <= a; ++i)
+            {
+                Console.WriteLine($"Введите номинал {i}-й карты");
+                string c = Console.ReadLine();
+                
+                switch (c)
+                {
+                    case "0":
+                        b += 0;
+                        break;
+
+                    case "1":
+                        b += 1;
+                        break;
+
+                    case "2":
+                        b += 2;
+                        break;
+
+                    case "3":
+                        b += 3;
+                        break;
+
+                    case "4":
+                        b += 4;
+                        break;
+
+                    case "5":
+                        b += 5;
+                        break;
+
+                    case "J":
+                        b += 10;
+                        break;
+
+                }
+                b += c;
+
+            }
+            
+            Console.WriteLine($"Сумма всех ваших карт = {b}");
+        }
+    }
+}
