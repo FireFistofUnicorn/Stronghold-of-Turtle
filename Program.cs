baa727d6990fca3f202eed3f1e20aa99fc4a0e74
FireFistofUnicorn <102551631+FireFistofUnicorn@users.noreply.github.com>
2022-05-15 14:22:04 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 
-namespace Lesson
+namespace CheckFullNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello ");
-            Console.Write("World");
-            Console.Write("!!!");
-            Console.ReadLine();
+            Console.WriteLine("Введите целое число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вы ввели число {n}");
+            
+            int i = 1;
+            while (i <= n - 1)
+            {
+                
+                double c = n / i;
+                bool g = c == 0;
+                if (g)
+                {
+
+                    Console.WriteLine($"К сожалению, ваше число {n} не является простым.");
+                    
+                }
+                else 
+                {
+                    Console.WriteLine($"Проздравляю, число {n} является простым");
+                }
+                Console.WriteLine(i);
+                i++;
+            }
         }
     }
 }
