4ef90d7d8f73923ae1a69ff0fd0aac017faf22f5
FireFistofUnicorn <102551631+FireFistofUnicorn@users.noreply.github.com>
2022-05-15 19:32:56 +0300

    Add files via upload

---
+++ CFN.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CFN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вы ввели число {n}");
+            int i = 2;
+            bool g = n % i == 0;
+
+            while (i <= n - 1)
+            {
+
+
+                if (g == true)
+                {
+
+                    Console.WriteLine($"К сожалению, ваше число {n} не является простым.");
+                }
+                else if (g == false)
+                {
+                    Console.WriteLine($"Проздравляю, число {n} является простым");
+                }
+                break;
+
+            }
+        }
+    }
+}
