5b9b659a2f79736b478d1c0bb820bedf55a0814e
FireFistofUnicorn <102551631+FireFistofUnicorn@users.noreply.github.com>
2022-07-15 11:39:33 +0300

    Add files via upload

---
+++ Mas1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write("Введите количество строк: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов: ");
+            int col = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int[,] a = new int[row, col];
+            Random r = new Random();
+            for (int i = 0; i < row; i++)
+            {
+                
+                for (int j = 0; j < col; j++)
+                {
+                    a[i,j] = r.Next(10);
+                    Console.Write($"{a[i, j]} ");
+                    
+                    for(int d = 0; d < 1; d++)
+                    {
+                        sum = sum + a[i,j];
+                    }
+                }
+                
+                
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Сумма всех элементов этой матрицы: {sum}");*/
+
+
+
+
+        }
+    }
+}
