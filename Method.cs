49bb0ab02dc1f498699605e8377e3df7652e938b
FireFistofUnicorn <102551631+FireFistofUnicorn@users.noreply.github.com>
2022-07-31 22:55:46 +0300

    Add files via upload

---
+++ Method.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            static string[] WordsSplitter(string text)
+            {
+                string[] wordsSplit = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string word = Console.ReadLine();
+                string[] words = new string[text.Length];
+                return words;
+
+
+                static void Main(string[] args)
+                {
+                    WordsSplitter(Console.ReadLine());
+                    foreach (char item in Console.ReadLine())
+                    {
+                        string[] wordd = new string[args.Length];
+                    }
+
+                    return;
+                }
+            }
+        }
+    }
+}
+
+            /*static string[] WordsSplitter(string text)
+            {
+                string[] wordsSplit = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string word = Console.ReadLine();
+                foreach (char item in word)
+                {
+                    string[] words = new string[text.Length];
+                }
+                return wordsSplit;
+
+            }
+
+            static void Main(string[] args)
+            {
+                WordsSplitter(Console.ReadLine());
+            */
+
