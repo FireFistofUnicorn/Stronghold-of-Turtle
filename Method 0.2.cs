83dda899ac96aee5bc1a816797e78a44f3dac6f6
FireFistofUnicorn <102551631+FireFistofUnicorn@users.noreply.github.com>
2022-08-01 15:53:14 +0300

    Add files via upload

---
+++ Method 0.2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathod_0._2
+{
+    internal class Program
+    {
+        /*static void Main(string[] args)
+        {
+            Words(args);
+            Console.WriteLine();
+        }
+        static string[] WordsSplitter(string text)
+        {
+            string[] wordsSplit = text.Split(' ');
+            return wordsSplit;
+        }
+
+        static string Words(string[] args)
+        {
+            string word = Console.ReadLine();
+            string[] words = WordsSplitter(word);
+            foreach (string word2 in words)
+            {
+                word = word2.Trim();
+                Console.WriteLine(word);
+            }
+            return word;
+        }*/
+
+        static void Main(string[] args)
+        {
+            string test2 = Console.ReadLine();
+            ReverseWord(args);
+            Console.WriteLine(test2);
+        }
+        static string[] SplitWord2(string text)
+        {
+            string[] wordsSplit2 = text.Split(' ');
+            ReverseWord(wordsSplit2);
+            return wordsSplit2;
+        }
+        static string ReverseWord(string[] text)
+        {
+            Array.Reverse(text);
+            string str = string.Join(" ", text);
+            return str;
+        }
+    }
+}
