b3de6b4f437f7ad4da8b049a0fb13f739b63d070
FireFistofUnicorn <102551631+FireFistofUnicorn@users.noreply.github.com>
2022-08-02 14:08:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CFN
+namespace Method_1._0
 {
-    class Program
+    internal class Program
     {
+        // Задание 1. Метод разделения строки на слова
+
+        /*static void Main(string[] args)
+        {
+            Words(args);
+            Console.WriteLine();
+        }
+        static string[] WordsSplitter(string text)
+        {
+            string[] wordsSplit = text.Split(' ');
+            return wordsSplit;
+        }
+
+        static string Words(string[] args)
+        {
+            string word = Console.ReadLine();
+            string[] words = WordsSplitter(word);
+            foreach (string word2 in words)
+            {
+                word = word2.Trim();
+                Console.WriteLine(word);
+            }
+            return word;
+        }*/
+
+        // Задание 2. Перестановка слов в предложении
+        static string ReverseWord(string[] args)
+        {
+            string word = Console.ReadLine();
+            string[] sReverse = SplitWord2(word);
+            Array.Reverse(sReverse);
+            SplitWord2(word);
+            foreach (string word2 in sReverse)
+            {
+                word = word2.Trim();
+                Console.Write(word + " ");
+            }
+            return word;
+        }
+        static string[] SplitWord2(string text)
+        {
+
+            string[] wordsSplit2 = text.Split(' ');
+            return wordsSplit2;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите целое число");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Вы ввели число {number}");
-            int counter = 2;
-
-            while (counter <= number - 1)
-            {
-
-                double remains = number % counter;
-                if (remains == 0)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"Проздравляю, число {number} является простым");
-                    break;
-                }
-            }
-            counter++;
+            ReverseWord(args);
+            Console.WriteLine();
         }
     }
 }
